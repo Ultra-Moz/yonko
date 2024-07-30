0e7ee80c9b9e2ba5679fb31722ec714e0ef593d1
Moizullah Shaikh <tufailshaikh10@gmail.com>
2024-07-30 21:31:03 +0500

    .

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import NameSection from "./Home/NameSection"
 
 const Home = () => {
-  return <div>Home</div>;
+  return <>
+  <NameSection/>
+  </>;
 };
 
 export default Home;
---
+++ src/Pages/Home/NameSection.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NameSection = () => {
+  return (
+    <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <div className='text-white'>
+            <h1>Karthik Deshmukh</h1>
+            <p>Unveiling our digital forge</p>
+        </div>
+    </div>
+  )
+}
+
+export default NameSection
