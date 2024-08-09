b23c1f19fc36b8cac2e1d9e4980cd903f7416d4b
Moizullah Shaikh <tufailshaikh10@gmail.com>
2024-08-09 14:45:47 +0500

    background lines fix

--- src/components/BackLines.jsx
+++ src/components/BackLines.jsx
@@ -3,14 +3,14 @@ import React from "react";
 const BackLines = () => {
   return (
     <>
-      <div className="min-h-screen w-full max-w-[1030px] fixed inset-y-0 grid grid-cols-2 md:grid-cols-3 gap-2 sm:gap-6">
-        <div className="w-full relative">
+      <div className="min-h-screen w-full max-w-[1078px] fixed inset-y-0 grid grid-cols-2 md:grid-cols-3 gap-2 sm:gap-6 px-6">
+        <div className={`w-full relative`}>
           <div className="absolute bg-[#1e1e1e50] w-0.5 min-h-screen right-0">
             <div className="bg-gradient-to-t from-transparent to-green absolute w-0.5 h-[200px] animate-[moveUp_7s_linear_infinite]"></div>
           </div>
           <div className="bg-[#1e1e1e50] w-0.5 min-h-screen"></div>
         </div>
-        <div className="w-full relative sm:hidden">
+        <div className="w-full relative">
           <div className="absolute bg-[#1e1e1e50] w-0.5 min-h-screen right-0"></div>
           <div className="bg-[#1e1e1e50] w-0.5 min-h-screen"></div>
         </div>
@@ -19,7 +19,7 @@ const BackLines = () => {
             <div className="bg-gradient-to-b from-transparent to-green absolute w-0.5 h-[200px] animate-[moveDown_7s_linear_infinite]"></div>
           </div>
           <div className="bg-[#1e1e1e50] w-0.5 min-h-screen absolute">
-            <div className="bg-gradient-to-t from-transparent to-orange absolute w-0.5 h-[200px] animate-[moveUp_7s_linear_infinite]"></div>
+            <div className="bg-gradient-to-t from-transparent to-orange absolute w-0.5 h-[200px] animate-[moveUp_7s_linear_infinite] hidden md:block"></div>
           </div>
         </div>
       </div>
