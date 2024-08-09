dd2f0481600ad764ee597944e2ba9fc5bf67da66
Moizullah Shaikh <tufailshaikh10@gmail.com>
2024-08-09 14:53:30 +0500

    extra component removed.

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -8,13 +8,11 @@ import BasedIn from "./BasedIn";
 import Services from "./Services";
 import Socials from "./Socials";
 import Testimonials from "./Testimonials";
-import BackgroundLines from "../../components/BackgroundLines";
 import BackLines from "../../components/BackLines";
 
 const Home = () => {
   return (
     <div className="w-full flex flex-col items-center h-full home px-6">
-      <BackgroundLines />
       <BackLines />
       <div className="wrapper w-full grid grid-cols-2 md:grid-cols-3 grid-row-7 xs:grid-row-5 relative gap-2 sm:gap-6 h-full">
         <NameSection />
